bba6f042558b9524b856fd20905b0b09e0a9b748
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-17 15:22:04 -0500

    made contact edit form

--- crm.rb
+++ crm.rb
@@ -17,7 +17,7 @@ get '/contacts/new' do
 end
 
 get '/contacts/:id' do
-  @contact = Contact.find_by(id: params[:id].to_i + 1)
+  @contact = Contact.find_by(id: params[:id])
   erb :show_contact
 end
 
@@ -25,6 +25,40 @@ get '/about' do
   erb :about_me
 end
 
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    note: params[:note]
+  )
+  redirect to ('/contacts')
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id])
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id])
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 after do
   ActiveRecord::Base.connection.close
